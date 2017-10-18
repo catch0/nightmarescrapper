0d3f38301a1626492bc21271c794b067e3974a14
Spence Peacock <spencerpeacock@gmail.com>
2017-10-18 15:07:01 -0500

    trying a different route

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
+var fs = require('fs');
+var path = require('path');
+var config = JSON.parse(fs.readFileSync("package.json"));
 var jquery = require('jquery');
 var Nightmare = require('nightmare');
       nightmare = Nightmare();
-      //bringing in twillio
-var accountSid = 'AC92bc1a2280f373b36fe52a516fe47845';
-var authToken = 'b409ac38d2cd9f72d3d74a0fb2226b68';
-var twilio = require('twilio')
-var client = new twilio(accountSid,authToken);
-// sending messages
-client.messages.create({
-  from: '+4698888235',
-  to: '+12142457207',
-  body: 'twilio works'
-}).then((message)=>console.log(message.sid));
+require('dotenv').load();
+//bringing in node mailer
+
+
+
+var account = process.env.ACCOUNT ;
+var pass = process.env.PASS ;
+
+
 
 
 var city = process.argv[2]
@@ -30,7 +31,7 @@ nightmare.goto('http://' + city + '.craigslist.org/search/bia?query=road&hasPic=
   //create the bike object with title and link then push it on in
   return bikes
 })
-.end()
+// .end()
 .then(function(result){
   for(bike in result){
     console.log(result[bike].title)
@@ -38,3 +39,6 @@ nightmare.goto('http://' + city + '.craigslist.org/search/bia?query=road&hasPic=
     console.log("\n")
   }
 })
+
+
+
